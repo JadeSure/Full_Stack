4addb706d5818cef9d2e0060974b3950949f4998
Shawn Wang <jadesure17@gmail.com>
2021-08-24 09:57:17 +1000

    pass all the test in: https://lazebear.github.io/jr-todos/

--- L10Homework.js
+++ L10Homework.js
@@ -7,62 +7,116 @@ app.use(cors)
 
 
 // middle ware
+// covert json to js object
 app.use(express.json()) 
 
 resList = []
+const generateId = generateID()
+const getItemID = () => generateId.next().value
+
+
+
 // task1
 app.post('/tasks', (req, res)=>{
-    resList.push(req.body)
+    const {description} = req.body
+    if(! description){
+        return res.status(400).json("descriptionn is missing")
+    }
+
+    const task = {id : getItemID(), description, done: false}
+    resList.push(task)
     // const temp = req.body
     // res.status(201).json({temp, description: "The task successfully added"})
-    res.status(201).json(req.body)
+    return res.status(201).json(task)
 })
 
 app.get('/tasks', (req, res)=>{
-    res.status(200).json(resList)
+
+    const {description} = req.query
+    if(description===undefined){
+        return res.status(200).json(resList)
+    }
+  
+    // resList.forEach(element =>{
+    //     if(element.description.includes(description)){
+
+    //         return res.json(element)
+    //     }else{
+    //         return res.status(400).json('cannot delete this ID')
+    //     }
+        
+    // })
+    const filtered = resList.filter(element => element.description.includes(description))
+    return res.json(filtered)
+    
 })
+
+
 
 app.get('/tasks/:id', (req,res)=>{
     const {id} = req.params
-    // const exist = false
+ 
+
     resList.forEach(element => {
-  
-        if (element.id == id){
+        
+        if (element.id === parseInt(id)){
             // exist = true
-            res.status(200).json(element)
-            
-        } 
+            task = element
+            return res.status(200).json(task)
+        }else{
+            return res.status(400).json('cannot find this ID')
+        }
+        
     });
+  
 
     // if (exist == false){
     //     res.status(404).json({description:"task not found"})
     // }
+    // const task = resList.find((item) => item.id === parseInt(id))
+    // res.json(task)
 
 })
 
 
 app.put('/tasks/:id', (req,res)=>{
     const {id} = req.params
+    const {description} = req.body
 
     for (let index = 0; index < resList.length; index++) {
         const element = resList[index];
         if (element.id == id){
-            resList[index] = req.body
-            res.status(200).json(req.body)
+            
+            resList[index] = {id:parseInt(id), description, done:true}
+            return res.status(200).json(req.body)
         }
     }
+    return res.status(400).json('cannot find this ID')
+
+
+
 })
 
 
 app.delete('/tasks/:id', (req,res)=>{
+   
     const {id} = req.params
-    for (let index = 0; index < resList.length; index++) {
-        const element = resList[index];
-        if (element.id == id){
-            resList.splice(index, 1)
-            res.status(204).json({description: "The task successfully deleted"})
+    let num =-1;
+    resList.forEach((item, index)=>{
+        if (item.id === parseInt(id)){
+            num = index
         }
+    })
+    // console.log(resList);
+    if(num != -1){
+        resList.splice(num, 1)
+
+        return res.status(204).json( "The task successfully deleted")
+    }else{
+        return res.status(400).json('cannot delete this ID')
     }
+  
+    
 })
 
 function cors(req, res, next){
@@ -72,6 +126,15 @@ function cors(req, res, next){
     next()
 }
 
+function* generateID(){
+    let id = 1
+    while(true) {
+        yield id++
+    }
+}
+
 app.listen(3000, () =>{
     console.log("server listening at point 3000");
 })
+
+
